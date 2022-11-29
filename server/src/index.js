6e981c34e0bb4629b617d3591cd3e517986aa7cf
Mario <mario1.lanpert@gmail.com>
2022-11-29 18:01:26 +0100

    Mario -> Lea

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,7 @@ const port = process.argv[2] || '8080';
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hallo Mario!');
+    res.end('Hallo Lea!');
 });
 
 server.listen(port, host, () => {
