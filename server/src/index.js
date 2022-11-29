c21e331784eef3d372e130cad7017476bd2c00d0
pandabacke <lxhc1809@hochschule-trier.de>
2022-11-29 18:27:58 +0100

    test hello w

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,7 @@ const port = process.argv[2] || '8080';
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!');
+    res.end('Hello W!');
 });
 
 server.listen(port, host, () => {
