03b6dd025d05c08771bf6f20a07a1b539217bfe0
Mario <mario1.lanpert@gmail.com>
2022-11-29 17:27:20 +0100

    init index.js and package.json

---
+++ server/src/index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const host = '127.0.0.1';
+const port = process.argv[2] || '8080';
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hallo Welt!');
+});
+
+server.listen(port, host, () => {
+    console.log(`Server laeuft http://${host}:${port}/`);
+});
