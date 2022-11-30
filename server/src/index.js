091a0b328298340f1ef8f0f9cda9a9e1aa9e2285
pandabacke <lxhc1809@hochschule-trier.de>
2022-11-30 18:10:44 +0100

    Added client files and connect index.html with client files

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,18 @@
 const http = require('http');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+app.use(express.static('build'));
+app.use('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', '..', 'build', 'index.html'));
+});
 
 const host = '127.0.0.1';
 const port = process.argv[2] || '8080';
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello W!');
-});
+const server = http.createServer(app);
 
 server.listen(port, host, () => {
-    console.log(`Server laeuft http://${host}:${port}/`);
+  console.log(`Server laeuft http://${host}:${port}/`);
 });
