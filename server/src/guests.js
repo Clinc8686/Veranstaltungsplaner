8387e7060753fad1ea47b5f1fe4199aae33d61ca
Mario <mario1.lanpert@gmail.com>
2022-12-02 22:48:42 +0100

    added more guests.js operations

--- server/src/guests.js
+++ server/src/guests.js
@@ -5,16 +5,44 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const router = express.Router();
 
 // Receive Post-Requests from index.html
-router.post('/guests', urlencodedParser, function (req, res, next) {
+router.post('/guests/insert', urlencodedParser, function (req, res, next) {
   // Insert Guest from Form into database
   console.log('guests!');
   const requestBody = req.body;
   const statement = 'INSERT INTO Guests (Name, Children, Invitationstatus) VALUES (?,?,?)';
   database.run(statement, [requestBody.name, requestBody.children, requestBody.invitationstatus], function (err, result) {
     if (err) throw err;
-    console.log('User dat is inserted successfully');
+    console.log('User was inserted successfully');
   });
 
+  // Redirect to index.html
+  res.redirect('/');
+});
+
+router.post('/guests/delete', urlencodedParser, function (req, res, next) {
+  // Insert Guest from Form into database
+  console.log('guests!');
+  const requestBody = req.body;
+  const statement = 'DELETE FROM Guests WHERE (ID = ?)';
+  database.run(statement, [requestBody.id], function (err, result) {
+    if (err) throw err;
+    console.log('User was deleted successfully');
+  });
+
+  // Redirect to index.html
+  res.redirect('/');
+});
+
+router.post('/guests/update', urlencodedParser, function (req, res, next) {
+  // Insert Guest from Form into database
+  const requestBody = req.body;
+  const statement = 'UPDATE Guests SET (Invitationstatus = ?) WHERE (ID = ?)';
+  database.run(statement, [requestBody.invitationstatus, requestBody.id], function (err, result) {
+    if (err) throw err;
+    console.log('User was updated successfully');
+  });
+
+  // Redirect to index.html
   res.redirect('/');
 });
 
