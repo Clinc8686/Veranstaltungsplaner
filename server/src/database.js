b1ed90244b50855181a7f03d29a83c2faac3e878
Mario <mario1.lanpert@gmail.com>
2022-12-08 17:48:08 +0100

    Added check for all values length > 0

--- server/src/database.js
+++ server/src/database.js
@@ -9,19 +9,19 @@ const db = new sqlite3.Database(path, (err) => {
 db.serialize(function () {
   // Create table
   db.run('CREATE TABLE IF NOT EXISTS `Events` (' +
-    '`ID` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,' +
-    '`Name` TEXT NOT NULL,' +
-    '`Datetime` DATETIME NOT NULL,' +
-    '`Guestlist` INTEGER NOT NULL,' +
-    '`Seatingplan` INTEGER NOT NULL' +
+    '`ID` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE CHECK(length(ID) > 0),' +
+    '`Name` TEXT NOT NULL CHECK(length(Name) > 0),' +
+    '`Datetime` DATETIME NOT NULL CHECK(length(Datetime) > 0),' +
+    '`Guestlist` INTEGER NOT NULL CHECK(length(Guestlist) > 0),' +
+    '`Seatingplan` INTEGER NOT NULL CHECK(length(Seatingplan) > 0)' +
     ')', (err) => {
     if (err) return console.log('Events: ' + err.message);
     console.log('Table Events created or exists');
   });
 
   db.run('CREATE TABLE IF NOT EXISTS `Guestlist` (' +
-    '`ID` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,' +
-    '`Guests` INTEGER NOT NULL,' +
+    '`ID` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE CHECK(length(ID) > 0),' +
+    '`Guests` INTEGER NOT NULL CHECK(length(Guests) > 0),' +
     'CONSTRAINT `Guestlist` FOREIGN KEY (`ID`) REFERENCES `Events` (`Guestlist`) ON DELETE CASCADE ON UPDATE CASCADE' +
     ')', (err) => {
     if (err) return console.log('Guestlist: ' + err.message);
@@ -29,10 +29,10 @@ db.serialize(function () {
   });
 
   db.run('CREATE TABLE IF NOT EXISTS `Guests` (' +
-    '`ID` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,' +
-    '`Name` TEXT NOT NULL UNIQUE,' +
-    '`Children` INTEGER NOT NULL,' +
-    '`Invitationstatus` VARCHAR(10) NOT NULL,' +
+    '`ID` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE CHECK(length(ID) > 0),' +
+    '`Name` TEXT NOT NULL UNIQUE CHECK(length(Name) > 0),' +
+    '`Children` INTEGER NOT NULL CHECK(length(Children) > 0),' +
+    '`Invitationstatus` VARCHAR(10) NOT NULL CHECK(length(Invitationstatus) > 0),' +
     'CONSTRAINT `Guests` FOREIGN KEY (`ID`) REFERENCES `Guestlist` (`Guests`) ON DELETE CASCADE ON UPDATE CASCADE' +
     ')', (err) => {
     if (err) return console.log('Guests: ' + err.message);
@@ -40,10 +40,10 @@ db.serialize(function () {
   });
 
   db.run('CREATE TABLE IF NOT EXISTS `Seatingplan` (' +
-    '`ID` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,' +
-    '`Tables` INTEGER NOT NULL,' +
-    '`Seats` INTEGER NOT NULL,' +
-    '`Onesided` BOOLEAN NOT NULL,' +
+    '`ID` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE CHECK(length(ID) > 0),' +
+    '`Tables` INTEGER NOT NULL CHECK(length(Tables) > 0),' +
+    '`Seats` INTEGER NOT NULL CHECK(length(Seats) > 0),' +
+    '`Onesided` BOOLEAN NOT NULL CHECK(length(Onesided) > 0),' +
     'CONSTRAINT `Seatingplan` FOREIGN KEY (`ID`) REFERENCES `Events` (`Seatingplan`) ON DELETE CASCADE ON UPDATE CASCADE' +
     ')', (err) => {
     if (err) return console.log('Seatingplan: ' + err.message);
