8b83bafefa1e2c4b06869b370c2acb0e2560e8bc
pandabacke <heckerlea@web.de>
2022-12-12 16:17:42 +0100

    added layout to homepage added and tested some dynamic page loads via buttons added comments

--- client/scripts/script.js
+++ client/scripts/script.js
@@ -41,4 +41,13 @@ window.onload = function () {
       console.log('not checked');
     }
   });
+  // Testing dynamic site load
+  document.getElementById('newGuestsButton').onclick = function () {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('guests').style.display = 'block';
+  };
+  document.getElementById('homeButton').onclick = function () {
+    document.getElementById('home').style.display = 'block';
+    document.getElementById('guests').style.display = 'none';
+  };
 };
