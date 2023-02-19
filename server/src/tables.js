77036a57fb198c750efc6c4ef3ed263ba4b2c6b7
Mario <mario1.lanpert@gmail.com>
2023-02-19 20:55:09 +0100

    remove Prepared Statements

--- server/src/tables.js
+++ server/src/tables.js
@@ -12,10 +12,10 @@ router.post('/tables/insert', urlencodedParser, function (req, res, next) {
   let statement;
   let params;
   if (requestBody.eventID) {
-    statement = database.prepare('INSERT INTO Seatingplan (ID, Tables, Seats, Onesided) VALUES (?,?,?,?);');
+    statement = 'INSERT INTO Seatingplan (ID, Tables, Seats, Onesided) VALUES (?,?,?,?);';
     params = [requestBody.eventID, requestBody.numberOfTables, requestBody.seatsPerTable, requestBody.twoSides];
   } else {
-    statement = database.prepare('INSERT INTO Seatingplan (Tables, Seats, Onesided) VALUES (?,?,?)');
+    statement = 'INSERT INTO Seatingplan (Tables, Seats, Onesided) VALUES (?,?,?)';
     params = [requestBody.numberOfTables, requestBody.seatsPerTable, requestBody.twoSides];
   }
 
@@ -44,7 +44,7 @@ router.post('/tables/insert', urlencodedParser, function (req, res, next) {
 router.get('/tables/select/:id', urlencodedParser, function (req, res, next) {
   // Get Tables from specific EventID
   const eventID = req.params.id;
-  const statement = database.prepare('SELECT Seatingplan.ID, Seatingplan.Tables, Seatingplan.Seats, Seatingplan.Onesided FROM `Seatingplan` INNER JOIN Events ON (Seatingplan.ID = Events.ID) WHERE Events.ID = ?;');
+  const statement = 'SELECT Seatingplan.ID, Seatingplan.Tables, Seatingplan.Seats, Seatingplan.Onesided FROM `Seatingplan` INNER JOIN Events ON (Seatingplan.ID = Events.ID) WHERE Events.ID = ?;';
   databaseAll(statement, res, eventID);
 });
 
@@ -52,7 +52,7 @@ router.post('/tables/update/:id', urlencodedParser, function (req, res, next) {
   // Update Seatingplan with specific ID
   const tableID = req.params.id;
   const requestBody = req.body;
-  const statement = database.prepare('UPDATE Seatingplan SET Tables = ?, Seats = ?, Onesided = ? WHERE ID = ?;');
+  const statement = 'UPDATE Seatingplan SET Tables = ?, Seats = ?, Onesided = ? WHERE ID = ?;';
   database.run(statement, [requestBody.numberOfTables, requestBody.seatsPerTable, requestBody.twoSides, tableID], function (err, result) {
     if (err) {
       throw err;
@@ -65,13 +65,13 @@ router.post('/tables/update/:id', urlencodedParser, function (req, res, next) {
 });
 
 router.get('/seats/select/:id', urlencodedParser, function (req, res, next) {
-  const statement = database.prepare('SELECT G.Name, GL.Seat, GL.Bench, GL.Guests FROM Events E INNER JOIN Guestlist GL ON GL.Events = E.ID INNER JOIN Guests G ON G.ID = GL.Guests WHERE Events = ?;');
+  const statement = 'SELECT G.Name, GL.Seat, GL.Bench, GL.Guests FROM Events E INNER JOIN Guestlist GL ON GL.Events = E.ID INNER JOIN Guests G ON G.ID = GL.Guests WHERE Events = ?;';
   const eventID = req.params.id;
   databaseAll(statement, res, eventID);
 });
 
 router.post('/seats/update', urlencodedParser, function (req, res, next) {
-  const statement = database.prepare('UPDATE `Guestlist` AS g1 SET `Seat` = g2.`Seat`, `Bench` = g2.`Bench` FROM `Guestlist` AS g2 WHERE g1.`Guests` = ? AND g2.`Guests` = ? AND g1.`Events` = g2.`Events` OR g1.`Guests` = ? AND g2.`Guests` = ? AND g1.`Events` = g2.`Events`;');
+  const statement = 'UPDATE `Guestlist` AS g1 SET `Seat` = g2.`Seat`, `Bench` = g2.`Bench` FROM `Guestlist` AS g2 WHERE g1.`Guests` = ? AND g2.`Guests` = ? AND g1.`Events` = g2.`Events` OR g1.`Guests` = ? AND g2.`Guests` = ? AND g1.`Events` = g2.`Events`;';
   const requestBody = req.body;
   database.run(statement, [requestBody.guestID1, requestBody.guestID2, requestBody.guestID2, requestBody.guestID1], function (err, result) {
     if (err) {
@@ -84,7 +84,7 @@ router.post('/seats/update', urlencodedParser, function (req, res, next) {
 });
 
 router.post('/seats/update/:id', urlencodedParser, function (req, res, next) {
-  const statement = database.prepare('UPDATE Guestlist SET Seat = ?, Bench = ? WHERE Guests = ? AND Events = ?;');
+  const statement = 'UPDATE Guestlist SET Seat = ?, Bench = ? WHERE Guests = ? AND Events = ?;';
   const requestBody = req.body;
   const userID = req.params.id;
   database.run(statement, [requestBody.seat, requestBody.bench, userID, requestBody.eventID], function (err, result) {
